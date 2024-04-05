3a25ae1e4c654e9e55b47460b56e77bbc889c7c2
juanjosepaz <juanjosepazga@gmail.com>
2024-04-05 11:51:34 -0500

    Bochik Character added

---
+++ Assets/_Scripts/Player/BochickMovement.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BochickMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb2D;
+
+    [Header("Movement")]
+    private float horizontalMovement = 0f;
+    [SerializeField] private float movementSpeed;
+    [Range(0, 0.3f)][SerializeField] private float movementDamping;
+    private Vector3 velocity = Vector3.zero;
+    private bool lookingRight = true;
+
+    [Header("Jump")]
+    [SerializeField] private float jumpForce;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private Vector3 boxDimentions;
+    [SerializeField] private bool isGrounded;
+    private bool jumpBool = false;
+
+    [Header("Animation")]
+    [SerializeField] private Animator animator;
+
+    private void Update()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal") * movementSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpBool = true;
+        }
+
+        animator.SetFloat("VerticalSpeed", Mathf.Clamp(rb2D.velocity.y, -1, 1));
+        animator.SetFloat("HorizontalSpeed", Mathf.Abs(rb2D.velocity.x));
+        animator.SetBool("isGrounded", isGrounded);
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapBox(groundCheck.position, boxDimentions, 0f, groundLayer);
+
+        Move(horizontalMovement * Time.fixedDeltaTime, jumpBool);
+
+        jumpBool = false;
+    }
+
+    private void Move(float movementDirection, bool jumpParameter)
+    {
+        Vector3 velocidadObjetivo = new Vector2(movementDirection, rb2D.velocity.y);
+        rb2D.velocity = Vector3.SmoothDamp(rb2D.velocity, velocidadObjetivo, ref velocity, movementDamping);
+
+        if (movementDirection > 0 && !lookingRight)
+        {
+            Turn();
+        }
+        else if (movementDirection < 0 && lookingRight)
+        {
+            Turn();
+        }
+
+        if (isGrounded && jumpParameter)
+        {
+            animator.SetTrigger("Jump");
+        }
+    }
+
+    public void Jump()
+    {
+        isGrounded = false;
+        rb2D.AddForce(new Vector2(0f, jumpForce));
+    }
+
+    private void Turn()
+    {
+        lookingRight = !lookingRight;
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + 180, 0);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(groundCheck.position, boxDimentions);
+    }
+}
